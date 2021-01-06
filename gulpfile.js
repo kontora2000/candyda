b631e74621dba3e45436ca9ff589c7f8e692f171
stasmyhero <49776649+stasmyhero@users.noreply.github.com>
2021-01-07 02:50:34 +0300

    fixes and burger

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,18 @@ var user = process.env.FTP_USER;
 var password = process.env.FTP_PASSWORD;
 function getFtpConnection(){
     return ftp.create({
-        host: '135.181.157.248',
+        host: '141.8.192.93',
         port: 21,
-        user: 'ftpadmin',
-        password: 'admin',
+        user: 'a0501694',
+        password: 'gpNocUKXvt',
         log: gutil.log,
     });
 }
 
-const remoteLocation = '/var/www/duma.one/';
+const remoteLocation = '/domains/duma.prostokontora.ru/public_html';
 gulp.task('remote-deploy', function(){
     var conn = getFtpConnection();
     return gulp.src(localFiles, {base: '.', buffer: false,})
-        .pipe(conn.newer(remoteLocation))
+        .pipe(conn.newerOrDifferentSize(remoteLocation))
         .pipe(conn.dest(remoteLocation))
 })
