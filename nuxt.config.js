8c106f05df22c368c9bc1bafce7af13f20b9d25c
stasmyhero <49776649+stasmyhero@users.noreply.github.com>
2021-01-08 23:27:44 +0300

    favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,17 @@ export default {
         meta: [
             { charset: 'utf-8', },
             { name: 'viewport', content: 'width=device-width, initial-scale=1', },
-            { hid: 'description', name: 'description', content: '', }
+            { hid: 'description', name: 'description', content: '', },
+            { name: 'msapplication-TileImage', content: '/icons/mstile-150x150.png', }
         ],
         link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', }
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', },
+            { rel: 'icon',  type: 'image/png',  sizes: '192x192', href:'/icons/android-chrome-192x192.png', },
+            { rel: 'icon',  type: 'image/png',  sizes: '512x512', href:'/icons/android-chrome-512x512.png', },
+            { rel: 'apple-touch-icon',  type: 'image/png',   href:'/icons/apple-touch-icon.png', },
+            { rel: 'icon',  type: 'image/png',  sizes: '16x16', href:'/icons/favicon-16x16.png', },
+            { rel: 'icon',  type: 'image/png',  sizes: '32x32', href:'/icons/favicon-32x32.png', },
+            { ref: 'manifest',  href: '/icons/site.webmanifest', }
         ],
     },
     /*
@@ -23,7 +30,6 @@ export default {
   ** Global CSS
   */
     css: [
-        { src: '~/static/reset.css', },
         { src: '~/static/style.css', }
     ],
     /*
