ca63d25ccd759d0788f54a09323f3d3b3282f4a4
stasmyhero <49776649+stasmyhero@users.noreply.github.com>
2021-02-08 15:21:42 +0300

    made page for districtts

---
+++ pages/region/_slug/index.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>
+
+ </div>
+</template>
+<script>
+import { defineComponent, } from '@nuxtjs/composition-api'
+import { useCandidateList, } from '~/composition/candidates'
+import { usePostList, } from '~/composition/posts'
+
+
+export default defineComponent({
+    layout: 'map',
+    transition: 'fade',
+    setup () {
+        const { candidates, } = useCandidateList()
+        const { posts, } = usePostList()
+        return {
+            candidates, 
+            posts,
+        }
+    },
+})
+</script>
