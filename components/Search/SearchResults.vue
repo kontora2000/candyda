f6111cb0fa31b582d91014cfafebdc7e4750d77a
stasmyhero <49776649+stasmyhero@users.noreply.github.com>
2021-03-16 15:13:29 +0300

    created component for search results

---
+++ components/Search/SearchResults.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="search-results-wrapper">
+    <div class="search-results-candidates" v-if="results.candidates">
+        <div class="search-result-header">{{ candidatesCount }}</div>
+        <div class="sarch-result-grid">
+            <CandidateCard v-for="candidate in results.candidates"
+            :key="candiate.id"
+            :candidate="candidate"></CandidateCard>
+        </div>
+    </div>
+    <div class="search-results-tags" v-if="results.tags">
+        <div class="search-result-header">{{ tagsCount }}</div>
+    </div> 
+    <div class="search-results-posts" v-if="results.posts">
+      <div class="search-result-header">{{  postsCount }}</div>
+    </div>
+  </div> 
+</template>
